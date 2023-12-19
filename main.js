249cd92e3a7fb6720e95afc98ca7bb36fbebe67c
Dest <149359862+YeMyat144@users.noreply.github.com>
2023-12-19 12:48:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-
-document.addEventListener('keydown', (event) => {
-    const playerSpeed = 10;
-
-    switch (event.key) {
-        case 'ArrowLeft':
-            movePlayer(player1, -playerSpeed);
-            break;
-        case 'ArrowRight':
-            movePlayer(player1, playerSpeed);
-            break;
-        case 'a':
-            movePlayer(player2, -playerSpeed);
-            break;
-        case 'd':
-            movePlayer(player2, playerSpeed);
-            break;
-        case 'Control':
-            attack(player1);
-            break;
-        case 'Shift':
-            attack(player2);
-            break;
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch player data and tournament schedule dynamically
+    fetchPlayerData();
+    fetchTournamentSchedule();
 });
 
-function movePlayer(player, speed) {
-    const playerPosition = player.offsetLeft + speed;
+function fetchPlayerData() {
+    // Example: Fetch player data from an API
+    // This is a simplified example; in a real scenario, you would make an AJAX request
+    const playerList = document.getElementById('player-list');
 
-    if (playerPosition >= 0 && playerPosition + player.offsetWidth <= window.innerWidth) {
-        player.style.left = playerPosition + 'px';
-    }
+    // Sample data for illustration
+    const players = ['Ryu', 'Chun-Li', 'Ken', 'Guile', 'Zangief'];
+
+    players.forEach(player => {
+        const listItem = document.createElement('li');
+        listItem.textContent = player;
+        playerList.appendChild(listItem);
+    });
 }
 
-function attack(player) {
-    player.classList.add('attack');
-    setTimeout(() => {
-        player.classList.remove('attack');
-    }, 500); // Adjust the duration of the attack animation
+function fetchTournamentSchedule() {
+    // Example: Fetch tournament schedule from an API
+    // This is a simplified example; in a real scenario, you would make an AJAX request
+    const scheduleTable = document.getElementById('schedule-table');
+
+    // Sample data for illustration
+    const schedule = [
+        { round: 'Round 1', time: '12:00 PM', location: 'Arena A' },
+        { round: 'Round 2', time: '2:00 PM', location: 'Arena B' },
+        { round: 'Semi-finals', time: '4:00 PM', location: 'Main Stage' },
+        { round: 'Finals', time: '6:00 PM', location: 'Main Stage' }
+    ];
+
+    schedule.forEach(match => {
+        const row = scheduleTable.insertRow();
+        row.insertCell(0).textContent = match.round;
+        row.insertCell(1).textContent = match.time;
+        row.insertCell(2).textContent = match.location;
+    });
 }
